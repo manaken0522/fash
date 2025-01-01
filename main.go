d8e47878ec78cec374b30125c337e099ff6289f9
manaken0522 <107743491+manaken0522@users.noreply.github.com>
2025-01-02 04:20:47 +0900

    ディレクトリのパス指定できるように

--- main.go
+++ main.go
@@ -3,24 +3,30 @@ package main
 import (
 	"crypto/md5"
 	"encoding/hex"
+	"flag"
 	"fmt"
-	"io/ioutil"
 	"os"
+	"path"
+	"path/filepath"
 	"slices"
-	"strings"
 )
 
 func main() {
-	extension := []string{"png", "jpeg", "mov", "webp", "gif", "mp4", "webm", "jpg"}
-	files, _ := ioutil.ReadDir("./")
+	folder_path := flag.String("p", "./", "folder path")
+	flag.Parse()
+
+	extensions := []string{".png", ".jpeg", ".mov", ".webp", ".gif", ".mp4", ".webm", ".jpg"}
+	files, _ := os.ReadDir(*folder_path)
 	for _, file := range files {
 		if !file.IsDir() {
-			if slices.Contains(extension, strings.Split(file.Name(), ".")[len(strings.Split(file.Name(), "."))-1]) {
-				file_bytes, _ := ioutil.ReadFile(file.Name())
+			file_path := path.Join(*folder_path, file.Name())
+			file_extension := filepath.Ext(file_path)
+			if slices.Contains(extensions, file_extension) {
+				file_bytes, _ := os.ReadFile(file_path)
 				hash_bytes := md5.Sum(file_bytes)
 				hash := hex.EncodeToString(hash_bytes[:16])
-				fmt.Println(file.Name(), fmt.Sprintf("%s.%s", hash, strings.Split(file.Name(), ".")[len(strings.Split(file.Name(), "."))-1]))
-				os.Rename(file.Name(), fmt.Sprintf("%s.%s", hash, strings.Split(file.Name(), ".")[len(strings.Split(file.Name(), "."))-1]))
+				fmt.Println(file_path, path.Join(*folder_path, fmt.Sprintf("%s%s", hash, file_extension)))
+				os.Rename(file_path, path.Join(*folder_path, fmt.Sprintf("%s%s", hash, file_extension)))
 			}
 		}
 	}
